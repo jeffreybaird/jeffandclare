01ea1b40dcdd05f3dc80988c6afecb058a041928
jeffreybaird <jlbaird87@gmail.com>
2013-07-02 08:56:28 -0400

    added things to do data

---
+++ data/things-to-do.rb
@@ -0,0 +1,202 @@
+
+
+{
+  :things_to_do =>
+  [{:category => 'Food',
+        :establishments =>[
+          {
+            :name => "El Caps",
+            :description => "Jeff’s favorite burger can be found in St. Petersburg.
+            It’s a little bit of a drive, but it’s worth it if you’retaking a trip out to the beaches."
+          },
+
+          {
+            :name =>"Wright’s Gourmet House",
+            :description => "This South Tampa staple is known for its sandwiches and cakes.
+            The desserts are Clare’s favorite part -- you might see a few at the wedding!"
+          },
+
+          {
+            :name => "Bern’s Steak House",
+            :description => "If you want a really nice night out,
+            this is your place. (It’s also Clare’s favorite restaurant.)
+            Bern’s usually serves local, organic food,
+            and it has the largest private wine collection in the country.
+            The Dessert Room is great if you are on more of a budget.
+            Take the complimentary tour if you go here!"
+          },
+
+          {
+            :name =>"Taco Bus",
+            :description =>"You read that right -- get your tacos from a bus.
+            The restaurant has expanded into a few locations,
+            but the original is a Tampa icon."
+          },
+
+          {
+            :name => "The Oxford Exchange Brunch",
+            :description => "lunch and tea are good,
+            but the ambiance and bookstore really make this place neat.
+            It’s only been open for about a year,
+            but it’s generated a lot of buzz. We took engagement photos here!"
+          },
+
+          {
+            :name =>"Datz and Datz Dough",
+            :description => "These are also South Tampa staples.
+            Datz is great for food or just drinks,
+            and it definitely attracts a younger crowd.
+            Dough is the spinoff bakery next door."
+          },
+
+          {
+            :name => "Hugo’s",
+            :description =>"If you want an authentic Cuban sandwich -- a Tampa specialty -- head to Hugo’s on Howard Avenue.
+            (Thanks to Colin Williams for this tip!)"
+          },
+
+          {
+            :name => "La Segunda Bakery",
+            :description => "This is the place for authentic Cuban bread and pastries."
+          },
+
+          {
+            :name => "Oystercatchers",
+            :description => "Delicious seafood and a great view of the Gulf of Mexico.
+            Do I really need to say anything else?"
+
+          },
+
+          {
+            :name => "The Columbia",
+           :description => "This historic restaurant in Ybor City, one of the oldest parts of Tampa, is delicious. You can’t go wrong with anything."
+          },
+
+          {
+           :name => "Mise en Place",
+           :description => "This is another place for a nice dinner.
+           It’s right near downtown and the University of Tampa."
+          },
+          {
+            :name => "Xtreme Juice",
+            :description => "If you like smoothies or need to feed a large group quickly,
+            this will change your life. It has turned me into a gigantic smoothie snob.
+            (Bet you didn’t know there was such a thing.)"
+          }]},
+
+   {:category => "Family activities",
+    :establishments =>[
+        {
+          :name => "Kate Jackson Park",
+          :description => "popular with local families for its fountains and jungle gym.
+          It’s also a short walk from Hyde Park Village,
+          which is great for shopping and dining."
+        },
+
+        {
+          :name => "Glazer Children’s Museum",
+          :description => "opened in the last couple years and has been getting a lot of great press."
+        },
+        {
+         :name => "Curtis Hixon Waterfront Park",
+         :description => "is just down the street from the children’s museum."
+        },
+
+        {
+          :name => "The Museum of Science and Industry (MOSI)",
+          :description => "is about 30 minutes north but has great exhibits and an IMAX dome movie theater."
+         },
+        {
+            :name => "Clearwater Marine Aquarium",
+            :description => "This is a small, not-for-profit aquarium, but you can get close to the animals. Also, “Dolphin Tale” was filmed here."
+        },
+        {
+          :name => "Florida Aquarium",
+          :description => "This has some great exhibits, including one on Florida wildlife. There’s also an outdoor playplace with fountains."
+        },
+        {
+            :name => "Lowry Park Zoo",
+            :description => "This is very kid-friendly, and the zoo has been updated and renovated in recent years."
+        }
+        ]},
+
+   {:category => "Outdoor activities",
+    :subtitle => "You might have heard we have beaches.",
+    :establishments =>[
+      {
+         :name => "Clearwater Beach",
+         :description => "is a little touristy in places but gorgeous and a pretty easy drive.
+         It’s consistently one of the country’s top-rated beaches.
+         St. Petersburg Beach is an easy drive and is a little quieter."
+      },
+      {
+        :name => "Weedon Island Preserve",
+        :description => "on Tampa Bay is great for kayaking, especially through the mangroves."
+
+      },
+      {
+        :name => "Fort DeSoto Park",
+        :description => "is a great beach that’s a little quieter and good for picnicking."
+      },
+      {
+        :name => "Caladesi Island",
+        :description => "is accessible by boat or ferry and has great nature trails and beaches."
+      },
+      {
+        :name => "Bayshore Boulevard",
+        :description => "is the longest continuous sidewalk in the world and is a Tampa icon.
+        This is where the Gasparilla parade is held every year, and you can walk to the
+        Jose Gaspar pirate ship. It’s popular for rollerblading, bicycling and dog-walking as well."
+      }]},
+
+   {:category => "City sightseeing",
+    :establishments =>[
+       {
+          :name => "The Dali Museum",
+          :description => "in St. Petersburg opened in its new location in the last couple years,
+          and it houses a huge collection on the waterfront."
+       },
+
+       {
+         :name => "The Tampa Museum of Art",
+         :description => "is near Curtis Hixon park on the Hillsborough River and houses mostly modern works."
+       },
+
+       {
+         :name => "The University of Tampa",
+         :description => " and the Henry B. Plant Museum are located on the Hillsborough River near downtown. The building used to be a hotel when Henry B. Plant was building railroads across Florida in the early 1900s.
+               Hyde Park Village is one of my favorite places for light shopping and snacking."
+       },
+
+       {
+         :name => "Ybor City",
+         :description =>" is the historic Cuban section of Tampa where cigar factories used to be located. Lots of red brick, small restaurants and quirky shops.
+          Waterfront St. Petersburg has small shops and cafes with a beautiful view of the marina."
+       },
+
+       {
+         :name => "Cigar City Brewing",
+         :description => "offers tours and tastings of great local beers that reflect the city’s Cuban roots."
+       }]},
+
+   {:category => "Nightlife",
+    :subtitle => "Howard Avenue is a good spot for the younger crowd",
+    :establishments =>[
+
+     {
+       :name => "MacDinton’s",
+       :description => "typical local 'hot spot'"
+     },
+     {
+       :name => "Dubliner",
+       :description => "typical local 'hot spot'"
+      },
+      {
+        :name => "King Corona Cigars",
+        :description => "in Ybor City is one of Jeff’s favorite places for handmade Tampa cigars and Cuban coffee."
+      },
+      {
+        :name => "Mad Dog’s and Englishmen",
+        :description =>" is a great little local pub that Jeff likes."
+      }]}]
+}
