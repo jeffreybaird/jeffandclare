9c6e269d3c0458f35481a26545a13081477c9d70
jeffreybaird <jlbaird87@gmail.com>
2013-07-02 13:33:17 -0400

    adding some better structure

--- app.rb
+++ app.rb
@@ -1,31 +1,36 @@
 require 'sinatra'
 require 'json'
 require "sinatra/activerecord"
+Dir.glob('./lib/*.rb') { |file| require file }
 
 
 set :database, "sqlite3:///rsvps.db"
 
 
-get '/' do
-  erb :show
-end
+class JeffAndClare < Sinatra::Base
+
+  get '/' do
+    erb :show
+  end
 
 
-post '/' do
-  # TODO: Read the message contents, save to the database
+  post '/' do
+    # TODO: Read the message contents, save to the database
 
-end
+  end
 
-get '/admin' do
-  erb :rsvp
-end
+  get '/admin' do
+    erb :rsvp
+  end
 
-helpers do
+  helpers do
 
-  def link_to(url,text=url,opts={})
-    attributes = ""
-    opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
-    "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+    def link_to(url,text=url,opts={})
+      attributes = ""
+      opts.each { |key,value| attributes << key.to_s << "=\"" << value << "\" "}
+      "<a href=\"#{url}\" #{attributes}>#{text}</a>"
+    end
+
   end
 
 end
