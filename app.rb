8360a328fb40c67c27500f6b2d77944c8c34d636
jeffreybaird <jlbaird87@gmail.com>
2013-07-04 23:03:46 -0400

    adding configurations

--- app.rb
+++ app.rb
@@ -1,19 +1,12 @@
 require 'sinatra'
 require 'json'
 require "sinatra/activerecord"
+require './config/environments'
 Dir.glob('./lib/*.rb') { |file| require file }
 
 
 # set :database, "sqlite3:///rsvps.db"
-db = URI.parse('postgres://jeff@localhost/jeff_and_clare_development')
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-  :host     => db.host,
-  :username => db.user,
-  :database => db.path[1..-1],
-  :encoding => 'utf8'
-)
 
 
 class JeffAndClare < Sinatra::Base
